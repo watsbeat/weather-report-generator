49acf5f4825b20c2ae9b5f5379686c1e6fb3442b
watsbeat <c.watsonbeaton@gmail.com>
2020-07-19 18:38:26 +1000

    add basic html-to-pdf code

--- api/v1/v1.ts
+++ api/v1/v1.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { apiCors } from './general/cors';
 import { logger } from './general/logging';
+import { generatePdf } from './buildReport';
 
 export let routerV1 = Router();
 
@@ -9,5 +10,6 @@ routerV1.use(logger);
 routerV1.use(apiCors);
 
 routerV1.get('/', (req, res, next) => {
+    generatePdf();
     res.send('Reports API');
 });
