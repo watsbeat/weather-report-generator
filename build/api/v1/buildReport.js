e480e0f3e312d40b3d186abddeb8cbf725709592
watsbeat <c.watsonbeaton@gmail.com>
2020-07-19 19:45:57 +1000

    build weather report

--- build/api/v1/buildReport.js
+++ build/api/v1/buildReport.js
@@ -7,10 +7,22 @@ const utils = require('util');
 const puppeteer = require('puppeteer');
 const hb = require('handlebars');
 const readFile = utils.promisify(fs.readFile);
+const axios = require('axios');
+async function getCurrentWeather() {
+    try {
+        const url = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02';
+        const response = await axios.get(url);
+        console.log(response.data);
+        return response.data;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 async function getTemplateHtml() {
     console.log('Loading template file in memory');
     try {
-        const reportPath = path.resolve('./templates/report.html');
+        const reportPath = path.resolve('./templates/weather-report.html');
         return await readFile(reportPath, 'utf8');
     }
     catch (err) {
@@ -19,7 +31,8 @@ async function getTemplateHtml() {
     }
 }
 async function generatePdf() {
-    let data = {};
+    const data = await getCurrentWeather();
+    console.log('data:', data);
     getTemplateHtml()
         .then(async (res) => {
         console.log('Compiling the template with handlebars');
